41598e74b1733a7214e23d6a4e1b79d0ab1b74ef
Fedora <raziel_lucio@icloud.com>
2019-04-24 22:02:07 -0500

    Agregando segundo decorador

--- decorators.py
+++ decorators.py
@@ -17,5 +17,19 @@ def password_required(func):
 def needs_password():
     print('La contrasena es correcta: ')
 
+def upper(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+
+        return result.upper()
+
+    return wrapper
+
+
+@upper
+def say_my_name(name):
+    return 'Hola, {}'.format(name)
+
 if __name__ == '__main__':
+    print(say_my_name('Raziel'))
     needs_password()
